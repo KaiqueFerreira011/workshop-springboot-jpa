bd0562eaf55200a43e420bd56677c71fa43233eb
Kaique Ferreira <kaique.ferreirakiko99@gmail.com>
2026-06-02 16:34:05 -0300

    Project created

---
+++ src/main/java/com/educandoweb/workshopspringbootjpa/WorkshopspringbootjpaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshopspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopspringbootjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopspringbootjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/workshopspringbootjpa/WorkshopspringbootjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshopspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringbootjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
