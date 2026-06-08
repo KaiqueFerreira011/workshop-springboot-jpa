01f01a642f83d47bca5b340eaf5e04a58df6321d
Kaique Ferreira <kaique.ferreirakiko99@gmail.com>
2026-06-08 16:06:20 -0300

    Category

--- src/main/java/com/educandoweb/workshopspringbootjpa/config/TestConfig.java
+++ src/main/java/com/educandoweb/workshopspringbootjpa/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.educandoweb.workshopspringbootjpa.config;
 
+import com.educandoweb.workshopspringbootjpa.entities.Category;
 import com.educandoweb.workshopspringbootjpa.entities.Order;
 import com.educandoweb.workshopspringbootjpa.entities.User;
 import com.educandoweb.workshopspringbootjpa.entities.enums.OrderStatus;
+import com.educandoweb.workshopspringbootjpa.repositories.CategoryRepository;
 import com.educandoweb.workshopspringbootjpa.repositories.OrderRepository;
 import com.educandoweb.workshopspringbootjpa.repositories.UserRepository;
 import java.time.Instant;
@@ -22,8 +24,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+        
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
