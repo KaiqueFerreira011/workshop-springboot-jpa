e6f4df6e93d57a04b3bbed0e9fff13bf66a36f60
Kaique Ferreira <kaique.ferreirakiko99@gmail.com>
2026-08-03 14:34:37 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/educandoweb/workshopspringbootjpa/entities/OrderItem.java
@@ -0,0 +1,101 @@
+package com.educandoweb.workshopspringbootjpa.entities;
+
+import com.educandoweb.workshopspringbootjpa.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+    
+    @EmbeddedId
+    private OrderItemPK id;
+    
+    private Integer quantity;
+    private Double price;
+    
+    public OrderItem() {
+        
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+    
+    public Product getProduct(){
+        return id.getProduct();
+    }
+    
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+    
+    public Order getOrder(){
+        return id.getOrder();
+    }
+    
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public OrderItemPK getId() {
+        return id;
+    }
+
+    public void setId(OrderItemPK id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        hash = 97 * hash + Objects.hashCode(this.quantity);
+        hash = 97 * hash + Objects.hashCode(this.price);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final OrderItem other = (OrderItem) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.quantity, other.quantity)) {
+            return false;
+        }
+        return Objects.equals(this.price, other.price);
+    }
+    
+    
+}
---
+++ src/main/java/com/educandoweb/workshopspringbootjpa/entities/pk/OrderItemPK.java
@@ -0,0 +1,63 @@
+package com.educandoweb.workshopspringbootjpa.entities.pk;
+
+import com.educandoweb.workshopspringbootjpa.entities.Order;
+import com.educandoweb.workshopspringbootjpa.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK {
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.order);
+        hash = 53 * hash + Objects.hashCode(this.product);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final OrderItemPK other = (OrderItemPK) obj;
+        if (!Objects.equals(this.order, other.order)) {
+            return false;
+        }
+        return Objects.equals(this.product, other.product);
+    }
+    
+    
+}
