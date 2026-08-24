fccb4b16d915c7756d3551134eebf51084a7a223
Kaique Ferreira <kaique.ferreirakiko99@gmail.com>
2026-08-24 14:03:26 -0300

    User insert

--- src/main/java/com/educandoweb/workshopspringbootjpa/services/UserService.java
+++ src/main/java/com/educandoweb/workshopspringbootjpa/services/UserService.java
@@ -9,16 +9,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository repository;
-    
+
     public List<User> findAll() {
         return repository.findAll();
     }
-    
-    public User findById(Long id){
-       Optional<User> obj = repository.findById(id);
-       return obj.get();
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
     }
 }
